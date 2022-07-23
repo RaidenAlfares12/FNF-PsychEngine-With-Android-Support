95f08e5bd2e81744a16893566f90f7d036c31ee2
Raiden Alfares 100% REAL <97826418+RaidenAlfares12@users.noreply.github.com>
2022-07-23 09:22:35 +0700

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -20,7 +20,9 @@ class CoolUtil
 	public static var defaultDifficulties:Array<String> = [
 		'Easy',
 		'Normal',
-		'Hard'
+		'Hard', 
+                'Hell',
+                'Super hell'
 	];
 	public static var defaultDifficulty:String = 'Normal'; //The chart that has no suffix and starting difficulty on Freeplay/Story Mode
 
